5e705f0d8337142fe674e4b4d74d53b5afac68ea
tarikbouari <tarikbouari@gmail.com>
2023-11-14 19:38:04 +0000

    Create component About

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => (
+  <div className="container mx-auto ">
+    <div className="w-full flex justify-start items-center ">
+
+      <h2 className="p-4 text-6xl"> About me</h2>
+      <hr className="text-[#F0F3FC]  w-[80%]" />
+    </div>
+  </div>
+);
+
+export default About;
