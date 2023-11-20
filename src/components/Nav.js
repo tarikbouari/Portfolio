77d5748e6d7dd13af29d45c010ad44f90d39fbb0
tarikbouari <tarikbouari@gmail.com>
2023-11-20 17:29:33 +0000

    Fixed nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,53 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { Link, animateScroll as scroll, scroller } from 'react-scroll';
 
 const Navbar = () => (
-  <div className=" w-full">
-    <nav className=" h-[70px] text-white flex justify-between items-center px-8">
+  <div className=" w-full fixed  bg-[#0A192F] ">
+    <nav className=" h-[70px] text-white flex justify-between items-center px-12 cursor-pointer">
       <span>TB</span>
       <ul className="flex justify-between w-60 ">
         <li>
           {' '}
-          <Link to="about text-2xl ">About</Link>
+          <Link
+            activeClass="active"
+            to="about"
+            spy
+            smooth
+            offset={-70}
+            duration={500}
+          >
+
+            About
+          </Link>
         </li>
         <li>
           {' '}
-          <Link to="work">Project</Link>
+          <Link
+            activeClass="active"
+            to="project"
+            spy
+            smooth
+            offset={-70}
+            duration={700}
+          >
+            Project
+          </Link>
         </li>
         <li>
           {' '}
-          <Link to="contact">Contact me</Link>
+          <Link
+            activeClass="active"
+            to="contact"
+            spy
+            smooth
+            offset={-90}
+            duration={700}
+          >
+            Contact me
+          </Link>
         </li>
       </ul>
     </nav>
