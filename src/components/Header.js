953cf50233c19994040f9efa76de3524f757fc28
tarikbouari <tarikbouari@gmail.com>
2023-11-20 17:28:10 +0000

    Resize header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
@@ -8,12 +9,21 @@ const Header = () => (
     <div className=" mx-auto  container px-6 ">
       <div className=" flex  mx-auto px-8 xl: ">
         <div className="text-white ">
-          <h1 className="text-2xl md:text-3xl lg:text-7xl xl:text-9xl  ">
-            {' '}
-            Hi, I'm
-            <span className="text-[#52eeca] ">  Tarik Bouari</span>
-          </h1>
-          <h2 className="text-2xl lg:text-7xl md:text-3xl  xl:text-9xl  ">  a Creative full-stack developer from Ghana</h2>
+          <h4 className="text-2xl md:text-3xl lg:text-7xl xl:text-8xl text-[#52eeca] ">
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Hi, I m Tarik Bouari',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                '', 1000,
+              ]}
+              wrapper="span"
+              speed={30}
+              style={{ display: 'block' }}
+              repeat={Infinity}
+            />
+          </h4>
+          <h2 className="text-2xl lg:text-7xl md:text-3xl  xl:text-9xl  ">  a Creative full-stack developer </h2>
         </div>
         <div className=" p-4 w-[20%]"> </div>
       </div>
