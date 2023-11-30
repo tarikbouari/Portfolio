d562db2a1623fbeb0e69cc598205561d0d63498a
tarikbouari <tarikbouari@gmail.com>
2023-11-30 20:37:38 +0000

    Updates

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-  <div className="w-full flex p-8">
-    <span className="mx-auto text-center text-[#a8b2d1]"> Designed & built by Tarik Bouari</span>
-  </div>
-);
-
-export default Footer;
--- src/components/work.js
+++
@@ -1,7 +0,0 @@
-const Work = () => (
-  <div>
-    <h1>.</h1>
-  </div>
-);
-
-export default Work;
