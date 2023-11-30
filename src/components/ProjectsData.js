8494bc664995de23903098435bc7662724b8f901
tarikbouari <tarikbouari@gmail.com>
2023-11-30 16:42:15 +0000

    Upadte project titre and description

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -6,7 +6,7 @@ import estate from '../images/vehii.jpg';
 const data = [
   {
     titre: 'NafexGlobal',
-    text: ' An ecommerce website for light and home decoration.Base in Accra, Nafex is specialized in providing exquisite lighting solutions that transform spaces, adding warmth, sophistication, and functionality. Designed and built using figma and WordPress   ',
+    text: ' An ecommerce website for light and home decoration base in Accra, Nafex is specialized in providing exquisite lighting solutions that transform spaces, adding warmth, sophistication, and functionality. Designed and built using figma and WordPress   ',
     image: ahoeimg,
     tools: { frist: 'GitHub', second: 'Figma', third: 'Wordpress' },
     links: { git: 'https://github.com/', source: 'www.nafexgh.com' },
@@ -28,13 +28,13 @@ const data = [
     links: { git: 'https://github.com/', source: 'www.ahoechic.com' },
   },
   {
-    titre: 'Gameland',
-    text: 'Gameland is an online gaming web application that allows users to view different kinds of games and share their opinions on each game. Users can like and make reservations to play online. Built with HTML/CSS, JAVASCRIPT, and  API.',
+    titre: 'AfroTech',
+    text: 'AfroTech festival is an online website for events and conferences about tech. AfroTech brings together all technology players such as startups, companies, and individuals. It also includes a conference where African business leaders and pioneers thinkers discuss the industrys most relevant issues. Afrotech is a place to share knowledge, build a growing network, exhibit the latest tech project and make business.. ',
     image: hap,
     tools: {
-      frist: 'HTML5/CSS', second: 'JavaScript', third: 'Webpack', fourth: 'API',
+      frist: 'HTML5/CSS', second: 'JavaScript', third: 'ES6',
     },
-    links: { git: 'https://github.com/', source: 'www.ahoechic.com' },
+    links: { git: 'https://github.com/tarikbouari/AfroTech-Festival', source: 'https://tarikbouari.github.io/AfroTech-Festival/index.html' },
   },
 
 ];
